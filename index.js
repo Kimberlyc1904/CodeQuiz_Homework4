99319e127d7bb9542ee6e445b0fb8ac592dc5099
kimberlyc <kimberlyc1904@yahoo.com>
2023-04-24 17:58:02 -0700

    adding changes to the javascript

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
+
+
+
 var questions =[
     {
-        title: "what is the color of the moon?",
-        choices:['red', 'green', 'blue', 'yellow' ],
-        answer: 'yellow',
+        title: "What does CSS stand for?",
+        choices:['Cascading Style Sheets', 'Cascading Single Sheets', 'Colored Style Sheets', 'Compound Single Sheets' ],
+        answer: 'Cascading Style Sheets',
 
     },
     {
-        title: "what is the size of the sea?",
-        choices:['small', 'big', 'huge', 'tiny' ],
-        answer: 'huge',
+        title: "Which of the following is not a primitive data type in JavaScript?",
+        choices:['Number', 'String', 'Object', 'Boolean' ],
+        answer: 'Object',
 
     },
+    {
+        title: "What does API stand for?",
+        choices:['Application Programming Interface', 'Automated Programming Interface', 'Application Processing Interface', 'Automated Processing Interface' ],
+        answer: 'Application Programming Interface',
 
+    },
+    {
+        title: "Which of the following is not a programming language?",
+        choices:['Python', 'JavaScript', 'HTML', 'CSS' ],
+        answer: 'HTML',
 
+    },
 
 
 ]
-
+var score
 var i =0
-var time =30
+var time =20
+function stopClock() {
+    clearInterval()
+}
 
 document.getElementById('btn').onclick=function() {
     showQuestion();
@@ -30,6 +46,16 @@ document.getElementById('btn').onclick=function() {
                 time --
                 document.getElementById('time').innerHTML = time
             }
+            else if(time<1) {
+                clearInterval();
+                //alert('Time is up!');
+            }
+            //else if(i === 4) {
+                //console.log("i=",i)
+                //clearInterval();
+                //score = time;
+                //alert("Your score is " + score);
+            //}
         }
         , 1000 //one second
     )
@@ -68,7 +94,32 @@ document.getElementById('questions').onclick = function(e) {
     }
     //goes to next question
         i++
+        if(i === 4) {
+            clearInterval()
+            score = time;
+        }
     showQuestion()
 
 }
 
+//need to 
+//saveButton.addEventListener("click", function(event) {
+    ///event.preventDefault();
+    
+    //var userScore = {
+      //initials: initials.value,
+      //score: score.value,
+    //};
+    
+    //localStorage.setItem("userScore", JSON.stringify(userScore));
+    //renderMessage();
+    
+   // });
+    
+    //function renderMessage() {
+      //var lastGrade = JSON.parse(localStorage.getItem("userScore"));
+      //if (lastGrade !== null) {
+       // document.querySelector(".message").textContent = lastGrade.student + 
+     // }
+   // }
+    
